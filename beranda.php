e5c19b8ecceaa16e04b86d67a866e1bc7e1dde84
viranzhra <viranurzahra12@gmail.com>
2024-07-04 13:22:46 +0700

    memperbarui salah satu isi file

--- beranda.php
+++ beranda.php
@@ -1,33 +1,35 @@
 <?php
-// Koneksi ke database
-$conn = mysqli_connect("localhost", "root", "", "proyek2_app");
+header('Content-Type: application/json');
 
-// Periksa koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+include 'koneksi.php';
+
+$koneksi = koneksi_database();
+$input = json_decode(file_get_contents('php://input'), true);
+
+$username = $input['username'];
+$password = $input['password'];
+
+// Mengambil nama siswa dan saldo mereka
+$query = "
+    SELECT m.nama_murid, IFNULL(SUM(a.saldo), 0) as total
+    FROM murid m
+    LEFT JOIN arsip_tabungan_bulanan a ON m.id = a.id_murid
+    WHERE m.username = ? AND m.password = ?
+    GROUP BY m.id
+";
+$stmt = mysqli_prepare($koneksi, $query);
+mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $nama, $total);
+mysqli_stmt_fetch($stmt);
+
+if ($nama && isset($total)) {
+    echo json_encode(['nama' => $nama, 'total' => $total]);
+} else {
+    http_response_code(404);
+    echo json_encode(['error' => 'Pengguna tidak ditemukan atau kredensial salah']);
 }
 
-// Ambil ID user yang login
-$user_id = $_SESSION['id'];
-
-// Query untuk mengambil nama user
-$query = "SELECT * FROM murid WHERE id = '$user_id'";
-$result = mysqli_query($conn, $query);
-
-// Periksa hasil query
-if (!$result) {
-    die("Query gagal: " . mysqli_error($conn));
-}
-
-// Ambil data dari hasil query
-$data = array();
-while ($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row;
-}
-
-// Tutup koneksi
-mysqli_close($conn);
-
-// Kembalikan data dalam bentuk JSON
-echo json_encode($data);
+mysqli_stmt_close($stmt);
+mysqli_close($koneksi);
 ?>
